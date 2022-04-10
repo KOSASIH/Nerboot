f33f358a71670b2ac57a30c5ef2a3147e89cfb4d
KOSASIH <kosasihg88@gmail.com>
2022-04-10 13:27:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-df8ss9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-df8ss9)
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+
+function Nerboot({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default Nerboot
